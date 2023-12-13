51f7e7309d9655f33f0ef1ff410e207e6d711be3
salemhe <salemsalemhe@gmail.com>
2023-12-13 19:35:53 +0100

    my second commit

--- Mern-client/src/shop/SingleBook.jsx
+++ Mern-client/src/shop/SingleBook.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const SingleBook = () => {
-    const {_id, bookTitle, imageUrl} = useLoaderData();
-  return (
-    <div className='mt-28 px-4 lg:px-24'>
-        <img src={imageUrl} alt="" className='h-96' />
-        <h2>{bookTitle}</h2>
-    </div>
-  )
-}
+  // Destructuring the data from useLoaderData
+  const { bookTitle, imageUrl, authorName, bookPdfUrl } = useLoaderData();
 
-export default SingleBook
+  const handleButtonClick = () => {
+    // Navigate to the specified URL when the button is clicked
+    window.open(bookPdfUrl, "_blank");
+  };
+
+  return (
+    <div className="mt-20 mx-auto px-4 lg:px-24 text-black">
+      {/* Container for book image, author name, book title, and download button */}
+      <div className="flex flex-col items-center mb-8">
+        {/* Display the book image */}
+        <img src={imageUrl} alt={bookTitle} className="h-80 mb-2 mx-auto" />
+
+        {/* Display the book details */}
+        <h2 className="text-lg font-bold">{bookTitle}</h2>
+        <h2 className="text-md font-bold">Author: {authorName}</h2>
+
+        {/* Use a button to trigger navigation */}
+        <button
+          className="text-blue-500 bg-transparent border border-solid border-blue-500 rounded p-1 mt-4"
+          onClick={handleButtonClick}
+        >
+          Download book here
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleBook;
